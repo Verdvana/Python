ac811b7e630f5beb32415a00c9d9c2e40a474497
verdvana <739197909@qq.com>
2024-08-05 22:34:43 +0800

    hello

--- hello/hello.py
+++ hello/hello.py
@@ -6,3 +6,18 @@ if int(number) > 100 :
     print('number>100')
 else:
     print('number<100')
+
+for number in range(1, 6):
+    if number > 3:
+        break
+    else:
+        print(number)
+else:
+    print("结束")
+
+i = 1
+
+while 1:
+    x = i*i
+    i = i+15
+    print (x)
